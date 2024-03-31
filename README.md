df4eb0236da75e7e02258d25ea0efec0ab459e6e
PelerYuan <pelerlittlepig@outlook.com>
2024-03-31 17:19:46 +0800

    frst version

---
+++ README.md
@@ -0,0 +1,19 @@
+AntiBydProtector
+===========
+
+![screenshot](./doc/screenshot.png)
+
+A program that allows you use the short key to close any suftware quickly and effortlessly.
+
+It's very useful when you want to play the computer in the classroom, and as soon as the teacher walk towards you, you can press the short key and close the game.
+
+based on the project <https://github.com/jarvisniu/KeyListener>, a background keyboard listener for .NET/C#, THANKS A LOT:)
+
+
+## Usage
+1. select the process that you want to kill after pressing the Key in the listbox below(you may need to spent sometime finding the process name of the software)
+2. if there are some new softwares be opened, just click refresh button
+3. press the `Ctrl + Q` to kill the process at any time you want
+
+## F&Q
+be free to send emails at peler_little_pig@outlook.com
---
+++ projects/KeyListener/WpfDemo/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using com.jarvisniu.utils;
+
+namespace WpfDemo
+{
+    public partial class MainWindow : Window
+    {
+        KeyListener keyListener = new KeyListener();
+
+        public MainWindow()
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            InitializeComponent();
+            keyListener.onPress("Ctrl+Q", onPress);   // combined key & multiple combination
+            FillProcessList();
+        }
+
+        private void onPress()
+        {
+            this.Dispatcher.Invoke(delegate
+            {
+                label1.Content = "keys pressed.";
+                foreach (string processName in processListBox.SelectedItems)
+                {
+                    // 获取所有正在运行指定名称的进程
+                    Process[] processes = Process.GetProcessesByName(processName.Trim());
+
+                    if (processes.Length > 0)
+                    {
+                        foreach (Process process in processes)
+                        {
+                            try
+                            {
+                                // 强制关闭进程
+                                process.Kill();
+                                Console.WriteLine($"已成功关闭进程：{process.ProcessName}");
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine($"关闭进程 '{process.ProcessName}' 时出现错误：{ex.Message}");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"没有找到名称为 '{processName}' 的进程。");
+                    }
+                }
+                this.Close();
+            });
+        }
+
+        private void FillProcessList()
+        {
+            try
+            {
+                // 获取所有正在运行的进程
+                Process[] processes = Process.GetProcesses();
+
+                // 将每个桌面应用程序的进程名添加到列表框中
+                foreach (Process process in processes)
+                {
+                    if (!string.IsNullOrEmpty(process.MainWindowTitle) && process.MainWindowHandle != IntPtr.Zero)
+                    {
+                        processListBox.Items.Add(process.ProcessName);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            processListBox.Items.Clear();
+            FillProcessList();
+        }
+        // end of class
+    }
+}
